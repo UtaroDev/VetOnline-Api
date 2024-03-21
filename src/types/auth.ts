2607e6d45871c603c2204dedf9fce9935d1cd8d9
lautaro ruciani <lautaroruciani@gmail.com>
2024-03-21 13:57:12 -0300

    update types for auth

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,28 +1,12 @@
 import type { z } from 'zod'
 import type {
-  bodyLoginSchema,
+  bodyAuthSchema,
   bodyEmailSchema,
   newPasswordTokenSchema
 } from '../schemas/auth'
-import type { BodyUserType } from './user'
+
 import type { JwtPayload } from 'jsonwebtoken'
-export type BodyLoginType = z.infer<typeof bodyLoginSchema>['body']
-
-export type BodyUserDataBaseType = BodyUserType & {
-  role: string
-  id: string
-  createdAt: Date
-  updatedAt: Date
-}
-
-export interface PayloadJwtType extends JwtPayload {
-  userFirstName: string
-  userRole: string
-  userId: string
-  refresh?: string
-  reset?: string
-  email?: string
-}
+export type BodyAuthType = z.infer<typeof bodyAuthSchema>['body']
 
 export type BodyEmailType = z.infer<typeof bodyEmailSchema>['body']
 
@@ -33,3 +17,19 @@ export type BodyNewPasswordTokenType = z.infer<
 export type ParamsNewPasswordTokenType = z.infer<
   typeof newPasswordTokenSchema
 >['params']
+
+export type BodyUserDataBaseType = BodyAuthType & {
+  id: string
+  role: string
+  resetToken?: string | null
+  resetTokenStatus?: boolean | null
+  createdAt: Date
+  updatedAt: Date
+}
+export interface PayloadJwtType extends JwtPayload {
+  userRole: string
+  userId: string
+  refresh?: string
+  reset?: string
+  email?: string
+}
