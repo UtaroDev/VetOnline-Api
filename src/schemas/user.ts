48964551902d25ac49b427a96bdac1714e5fa8ee
lautaro ruciani <lautaroruciani@gmail.com>
2024-03-21 13:57:38 -0300

    update auth schemas

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,36 +1,48 @@
+// Validacion pensada para el perfil del usuario
 import { z } from 'zod'
 
-export const bodyUserSchema = z.object({
+export const bodyProfileSchema = z.object({
   body: z.object({
     firstName: z
       .string()
-      .min(2)
-      .max(255)
+      .min(2, {
+        message: 'El nombre debe contener al menos 2 letras'
+      })
+      .max(255, {
+        message: 'El nombre solo puede contener 255 letras'
+      })
+      .transform((value) => value.trim())
       .refine((value) => /^[a-zA-Z]+$/.test(value), {
         message:
           'El nombre solo puede contener letras de a a z (mayúsculas o minúsculas).'
-      })
-      .transform((value) => value.trim()),
+      }),
 
     lastName: z
       .string()
-      .min(2)
-      .max(255)
+      .min(2, {
+        message: 'El apellido debe contener al menos 2 letras'
+      })
+      .max(255, {
+        message: 'El apellido solo puede contener 255 letras'
+      })
+      .transform((value) => value.trim())
       .refine((value) => /^[a-zA-Z]+$/.test(value), {
         message:
           'El apellido solo puede contener letras de a a z (mayúsculas o minúsculas).'
-      })
-      .transform((value) => value.trim()),
+      }),
 
     email: z
       .string()
-      .email()
+      .email({ message: 'Asegurate de introducir un mail valido' })
       .transform((value) => value.trim()),
-
     password: z
       .string()
-      .min(8)
-      .max(16)
+      .min(8, {
+        message: 'La contraseña debe contener al menos 8 caracteres'
+      })
+      .max(16, {
+        message: 'La contraseña no puede sueperar los 16 caracteres'
+      })
       .refine(
         (value) =>
           /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]+$/.test(value),
@@ -44,24 +56,25 @@ export const bodyUserSchema = z.object({
       .transform((value) => value.trim())
       .refine((value) => /^\d{6,12}$/.test(value), {
         message: 'El número de teléfono debe contener entre 6 y 12 dígitos.'
-      })
-      .transform((value) => value.trim()),
-
+      }),
     address: z
       .string()
-      .min(2)
-      .max(255)
-      .transform((value) => value.trim())
+      .min(2, {
+        message: 'La direccion debe contener al menos 2 caracteres'
+      })
+      .max(255, {
+        message: 'La direccion no puede superar los 255 caracteres'
+      })
   })
 })
 
-export const idUserSchema = z.object({
+export const idProfileSchema = z.object({
   params: z.object({
     id: z.string().length(24)
   })
 })
 
 export const updateUserSchema = z.object({
-  params: idUserSchema.shape.params,
-  body: bodyUserSchema.shape.body
+  params: idProfileSchema.shape.params,
+  body: bodyProfileSchema.shape.body
 })
