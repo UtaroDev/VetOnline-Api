3195ce854fa4cc0c51d656afdb26222af6b71c48
lautaro ruciani <lautaroruciani@gmail.com>
2024-03-21 13:58:00 -0300

    create new methods in auth services

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,15 +1,23 @@
 // Iria registro y login
 import { PrismaClient } from '@prisma/client'
-
-import type { BodyUserType } from '../types/user'
-
+import type { BodyAuthType } from '../types/auth'
 const prisma = new PrismaClient()
 
 export const authService = {
-  register: async (data: BodyUserType) => {
+  register: async (data: BodyAuthType) => {
     return await prisma.user.create({ data })
   },
-
+  updateAuthData: async (email: string, newData: BodyAuthType) => {
+    return await prisma.user.update({
+      where: { email },
+      data: newData
+    })
+  },
+  deleteAccount: async (email: string) => {
+    return await prisma.user.delete({
+      where: { email }
+    })
+  },
   findEmail: async (email: string) => {
     return await prisma.user.findUnique({
       where: {
