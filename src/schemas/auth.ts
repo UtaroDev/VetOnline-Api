7247273c8361f95cf58ff283a9b4f070ebc17e40
lautaro ruciani <lautaroruciani@gmail.com>
2024-03-21 13:57:24 -0300

    update auth schemas

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,5 +1,6 @@
+// Esquema de validacion para y solo para el registro y logeo del usuario
 import { z } from 'zod'
-export const bodyLoginSchema = z.object({
+export const bodyAuthSchema = z.object({
   body: z.object({
     email: z
       .string()
@@ -7,14 +8,27 @@ export const bodyLoginSchema = z.object({
         message: 'Asegurate de que el email sea valido'
       })
       .transform((value) => value.trim().toLowerCase()),
-    password: z.string().min(8, {
-      message: 'La contraseña debe tener al menos 8 caracteres'
-    })
+    password: z
+      .string()
+      .min(8, {
+        message: 'La contraseña debe tener al menos 8 caracteres'
+      })
+      .max(16, {
+        message: 'La contraseña no debe tener mas de 16 caracteres'
+      })
+      .refine(
+        (value) =>
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]+$/.test(value),
+        {
+          message:
+            'La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número.'
+        }
+      )
   })
 })
 
 export const bodyEmailSchema = z.object({
-  body: bodyLoginSchema.shape.body.pick({ email: true })
+  body: bodyAuthSchema.shape.body.pick({ email: true })
 })
 
 // Esquema para enpoind donde se crea una nueva contraseña con token
@@ -22,5 +36,5 @@ export const newPasswordTokenSchema = z.object({
   params: z.object({
     token: z.string()
   }),
-  body: bodyLoginSchema.shape.body.pick({ password: true })
+  body: bodyAuthSchema.shape.body.pick({ password: true })
 })
