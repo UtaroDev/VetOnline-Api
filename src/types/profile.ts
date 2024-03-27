480e4d0bb93a5730243020e03d78c05d8bd70f7e
lautaro ruciani <lautaroruciani@gmail.com>
2024-03-27 20:18:50 -0300

    update type

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,7 +1,11 @@
 import type { z } from 'zod'
 
-import type { bodyProfileSchema, idProfileSchema } from '../schemas/profile'
+import type {
+  bodyProfileSchema,
+  idProfileSchema,
+  updateBodyUserSchema
+} from '../schemas/profile'
 
+export type BodyUpdateProfileType = z.infer<typeof updateBodyUserSchema>['body']
 export type BodyProfileType = z.infer<typeof bodyProfileSchema>['body']
-
 export type IdProfileType = z.infer<typeof idProfileSchema>['params']
